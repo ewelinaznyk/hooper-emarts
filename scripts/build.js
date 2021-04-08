8e4c850b4adeb0dfd07f200b84f4aaf40a6077bb
Michal Szajnecki <michal.szajnecki@gmail.com>
2021-04-08 17:34:52 +0200

    fixed problem with mkdirp package

--- scripts/build.js
+++ scripts/build.js
@@ -6,22 +6,20 @@ const chalk = require('chalk');
 const uglify = require('uglify-js');
 const mkdirpNode = require('mkdirp');
 const { rollup } = require('rollup');
-const { promisify } = require('util');
 const { configs, paths, common } = require('./config');
-
-const mkdirp = promisify(mkdirpNode);
 
 build('umd');
 build('esm');
 
-async function build (build) {
-  await mkdirp(paths.dist);
-  const config = configs[build]; 
+async function build(build) {
+  console.log(paths.dist);
+  await mkdirpNode(paths.dist);
+  const config = configs[build];
   console.log(chalk.cyan(`Generating ${build} build...`));
   const bundle = await rollup(config.input);
   const { output } = await bundle.generate(config.output);
   const code = output[0].code;
-  
+
   let fileName = `${common.name}${config.ext}.js`;
   let outputPath = path.join(paths.dist, fileName);
 
@@ -40,7 +38,7 @@ async function build (build) {
   console.log(`${chalk.green(`Output File: ${fileName}`).padEnd(40, ' ')} ${stats}`);
 }
 
-function getStats ({ path, code }) {
+function getStats({ path, code }) {
   const { size } = fs.statSync(path);
   const gzipped = gzipSize.sync(code);
 
