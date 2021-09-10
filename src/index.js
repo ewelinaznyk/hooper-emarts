d877576c0500f89f5bbb9ad526035e2fe40c730a
Ewelina Znyk <ewelina.znyk@gmail.com>
2021-09-10 12:58:36 +0200

    remove progress component

--- src/index.js
+++ src/index.js
@@ -2,10 +2,9 @@ import Carousel from './Carousel';
 import Slide from './Slide';
 import addonMixin from './addons/Mixin';
 import Icon from './addons/Icon';
-import Progress from './addons/Progress';
 import Pagination from './addons/Pagination';
 import Navigation from './addons/Navigation';
 
-export { Carousel as Hooper, Slide, Progress, Pagination, Navigation, Icon, addonMixin };
+export { Carousel as Hooper, Slide, Pagination, Navigation, Icon, addonMixin };
 
 export default Carousel;
